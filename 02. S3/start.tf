e2112cf0253340e980d485823e83d6da8d6bfa2a
k2 <trofimchuk.an@gmail.com>
2022-05-04 14:19:19 +0300

    S3

--- 02. S3/start.tf
+++ 02. S3/start.tf
@@ -7,7 +7,27 @@ provider "aws" {
 resource "aws_s3_bucket" "rivne_itstep" {
   bucket = "s3-rivne.itstep"
   acl    = "public-read"
+
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+
   versioning {
     enabled = true
   }
 }
+
+# resource "aws_s3_bucket_object" "html" {
+#   for_each = fileset("../site/", "**/*.html")
+
+#   bucket       = aws_s3_bucket.rivne_itstep
+#   key          = each.value
+#   source       = "../site/${each.value}"
+#   etag         = filemd5("../site/${each.value}")
+#   content_type = "text/html"
+# }
+
+
